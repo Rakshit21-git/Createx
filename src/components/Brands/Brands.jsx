919a31dbcb20276699e56c361010a1613929715f
Khushboo Gehlot <khushbugehlot200@gmail.com>
2023-09-06 17:02:38 +0530

    modified

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Brands = () => {
     return (
         <>
-            <div className="container mt-5">
+            <div className="container mt-5 mb-5">
                 <div className="row">
                     <div className="col-6 col-md-4 col-lg-2 col-xl-2">
                         <img className='images' src="./Assests/Images/brandlogo1.png" alt="not available" />
